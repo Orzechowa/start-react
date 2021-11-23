52573c7e74f31eaaba5237acc7fb0670a56bb2fa
Ewa Orzechowska <orzechowska1989@wp.pl>
2021-11-23 22:36:03 +0100

    add card

---
+++ src/components/Card/Card.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './Card.scss';
+import PropTypes from 'prop-types';
+
+
+class Card extends React.Component {
+    render() {
+      return (
+        <section className={styles.component}>
+            {this.props.title}
+        </section>
+      );
+    };
+};
+Card.propTypes = {
+    title: PropTypes.node.isRequired,
+};
+
+export default Card;
