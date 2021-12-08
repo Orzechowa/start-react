cea19217b885cb74a9427461c5e39313638239d1
Ewa Orzechowska <orzechowska1989@wp.pl>
2021-12-08 23:50:05 +0100

    add correction

--- src/redux/searchStringRedux.js
+++ src/redux/searchStringRedux.js
@@ -1,4 +1,3 @@
-import shortid from 'shortid';
 // selectors
 export const getSearchString = ({searchString}) => searchString;
 
@@ -15,7 +14,7 @@ const createActionName = name => `app/${reducerName}/${name}`;
 
 export const CHANGE = createActionName('CHANGE');
 // action creators
-export const createAction_changeSearchString = payload => ({ payload: { ...payload, id: shortid.generate() }, type: CHANGE });
+export const createAction_changeSearchString = payload => ({ payload, type: CHANGE });
 // reducer
 export default function reducer(statePart = '', action = {}) {
   switch (action.type) {
