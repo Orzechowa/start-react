f21fc2d04c5c122bc76f0c5d4c1ac57a471d6dba
Ewa Orzechowska <orzechowska1989@wp.pl>
2021-12-12 23:54:32 +0100

    add container

---
+++ src/components/Container/Container.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './Container.scss';
+import PropTypes from 'prop-types';
+
+class Container extends React.Component {
+  render(){
+    return (
+      <div className={styles.component}>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+Container.propTypes = {
+  children: PropTypes.node,
+};
+
+export default Container;
